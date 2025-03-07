da6f4e0483d5e054432d1c454ef038b088eb38f9
kavitosik <km220410@gmail.com>
2025-03-07 21:44:50 +0300

    работа на уроке

---
+++ my_project/main/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    host = request.META["HTTP_HOST"]
+    user_agent = request.META["HTTP_USER_AGENT"]
+    path = request.path
+
+    return HttpResponse(f"""
+        <p>Host: {host}</p>
+        <p>Path: {path}</p>
+        <p>User-agent: {user_agent}</p>
+    """)
